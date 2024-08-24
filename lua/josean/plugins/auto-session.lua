fcd144f33b51b20fd49c813e81433b99b0779cc9
aayu-11 <ayushkumar.ak2409@gmail.com>
2024-08-24 18:08:29 +0530

    few changes

--- lua/josean/plugins/auto-session.lua
+++ lua/josean/plugins/auto-session.lua
@@ -1,16 +1,25 @@
 return {
-  "rmagatti/auto-session",
-  config = function()
-    local auto_session = require("auto-session")
+	"rmagatti/auto-session",
+	config = function()
+		local auto_session = require("auto-session")
 
-    auto_session.setup({
-      auto_restore_enabled = false,
-      auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
-    })
+		auto_session.setup({
+			auto_restore_enabled = false,
+			auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
+			session_lens = {
+				buftypes_to_ignore = {}, -- Filetypes that should not be included in a session
+				load_on_setup = true, -- When SessionLoad is called, should it load the session?
+				theme_conf = { border = true }, -- Options for session lens border and title
+				previewer = true, -- Show preview of session on hover
+			},
+			vim.keymap.set("n", "<leader>ls", require("auto-session.session-lens").search_session, {
+				noremap = true,
+			}),
+		})
 
-    local keymap = vim.keymap
+		local keymap = vim.keymap
 
-    keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
-    keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
-  end,
+		keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
+		keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
+	end,
 }

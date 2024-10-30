d1cad1a0b583e9c6157e5c12e2efccae4bac1ab9
aayu-11 <ayushkumar.ak2409@gmail.com>
2024-10-30 19:01:22 +0530

    noice plugin added for popups

--- lua/josean/plugins/auto-save.lua
+++ lua/josean/plugins/auto-save.lua
@@ -5,18 +5,10 @@ return {
 		event = { "BufLeave", "FocusLost" }, -- Changed to only trigger on focus changes
 		opts = {
 			enabled = true,
-			execution_message = {
-				enabled = true,
-				message = function()
-					return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
-				end,
-				dim = 0.18,
-				cleaning_interval = 1250,
-			},
 			trigger_events = {
 				immediate_save = { "BufLeave", "FocusLost" }, -- These are the only events that will trigger saves
 				defer_save = {}, -- Removed InsertLeave and TextChanged
-				cancel_defered_save = {}, -- Removed InsertEnter since we're not using deferred saves
+				-- cancel_defered_save = {}, -- Removed InsertEnter since we're not using deferred saves
 			},
 			condition = nil,
 			write_all_buffers = false,
